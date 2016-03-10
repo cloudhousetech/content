19c2f0f622fe6eba727ed6dad34f8560cbb00b6a
Alistair Pialek <alistair.pialek@scriptrock.com>
2016-03-09 21:11:43 -0800

    updated logging

--- puppet/upguard.rb
+++ puppet/upguard.rb
@@ -4,10 +4,7 @@ require 'erb'
 
 Puppet::Reports.register_report(:upguard) do
 
-  Puppet.notice("upguard: notice: test message")
-  Puppet.info("upguard: info: test message")
-  Puppet.debug("upguard: debug: test message")
-  Puppet.err("upguard: err: test message")
+  Puppet.info("upguard: starting report processor")
 
   desc "Create a node (if not present) and kick off a node scan in UpGuard if changes were made."
 
@@ -16,7 +13,7 @@ Puppet::Reports.register_report(:upguard) do
   begin
     config = YAML.load_file(configfile)
   rescue TypeError => e
-    raise Puppet::ParserError, "upguard.yaml file is invalid"
+    raise(Puppet::ParserError, "upguard.yaml file is invalid")
   end
 
   APPLIANCE_URL = config[:appliance_url]
@@ -25,35 +22,44 @@ Puppet::Reports.register_report(:upguard) do
   API_KEY = "#{SERVICE_KEY}#{SECRET_KEY}"
   WINDOWS_CM_GROUP_ID = config[:windows_cm_group_id]
 
+  Puppet.info("upguard: APPLIANCE_URL=#{APPLIANCE_URL}")
+  Puppet.info("upguard: SERVICE_KEY=#{SERVICE_KEY}")
+  Puppet.info("upguard: SECRET_KEY=#{SECRET_KEY}")
+  Puppet.info("upguard: API_KEY=#{API_KEY}")
+  Puppet.info("upguard: WINDOWS_CM_GROUP_ID=#{WINDOWS_CM_GROUP_ID}")
+
   def process
 
-    Puppet.notice("upguard: notice: test message")
-    Puppet.info("upguard: info: test message")
-    Puppet.debug("upguard: debug: test message")
-    Puppet.err("upguard: err: test message")
-
     self.status != nil ? status = self.status : status = 'undefined'
+
+    Puppet.info("upguard: status=#{status}")
 
     if status == 'changed'
       node_ip_hostname = self.host
       manifest_filename = ERB::Util.url_encode("#{self.logs.first.file.truncate(45)}")
 
+      Puppet.info("upguard: node_ip_hostname=#{node_ip_hostname}")
+      Puppet.info("upguard: manifest_filename=#{manifest_filename}")
+
       lookup = node_lookup(API_KEY, APPLIANCE_URL, node_ip_hostname)
 
       if lookup["node_id"]
         node_id = lookup["node_id"]
+        Puppet.info("upguard: node found: node_id=#{node_id}")
       elsif lookup["error"] == "Not Found"
         node_id = node_create(API_KEY, APPLIANCE_URL, node_ip_hostname, WINDOWS_CM_GROUP_ID)
+        Puppet.info("upguard: node not found so created: node_id=#{node_id}")
       else
-        Puppet.notice("upguard: failed to lookup node: #{lookup}")
+        Puppet.err("upguard: failed to lookup node: #{lookup}")
+        raise StandardError, "upguard: unable to get a node id: confirm config variables are correct and appliance is reachable"
       end
 
       job = node_scan(API_KEY, APPLIANCE_URL, node_id, manifest_filename)
 
       if job["job_id"]
-        Puppet.notice("upguard: node scan kicked off against #{node_ip_hostname} (#{APPLIANCE_URL}/jobs/#{job["job_id"]}/show_job?show_all=true)")
+        Puppet.info("upguard: node scan kicked off against #{node_ip_hostname} (#{APPLIANCE_URL}/jobs/#{job["job_id"]}/show_job?show_all=true)")
       else
-        Puppet.notice("upguard: failed to kick off node scan against #{node_ip_hostname} (#{node_id}): #{job}")
+        Puppet.err("upguard: failed to kick off node scan against #{node_ip_hostname} (#{node_id}): #{job}")
       end
     end
   end
@@ -61,6 +67,7 @@ Puppet::Reports.register_report(:upguard) do
   # Check to see if the node has already been added to UpGuard. If so, return it's node_id.
   def node_lookup(api_key, instance, external_id)
     response = `curl -X GET -s -k -H 'Authorization: Token token="#{api_key}"' -H 'Accept: application/json' -H 'Content-Type: application/json' #{instance}/api/v2/nodes/lookup.json?external_id=#{external_id}`
+    Puppet.info("upguard: node_lookup response=#{response}")
     JSON.load(response)
   end
   module_function :node_lookup
@@ -69,11 +76,13 @@ Puppet::Reports.register_report(:upguard) do
   def node_create(api_key, instance, ip_hostname, windows_cm_group_id)
     node_details = '{ "node": { "name": ' + "\"#{ip_hostname}\"" + ', "short_description": "Added via the API.", "node_type": "SV", "operating_system_family_id": 1, "operating_system_id": 125, "medium_type": 7, "medium_port": 5985, "connection_manager_group_id": ' + "\"#{windows_cm_group_id}\"" + ', "medium_hostname": ' + "\"#{ip_hostname}\"" + ', "external_id": ' + "\"#{ip_hostname}\"" + '}}'
     response = `curl -X POST -s -k -H 'Authorization: Token token="#{api_key}"' -H 'Accept: application/json' -H 'Content-Type: application/json' -d '#{node_details}' #{instance}/api/v2/nodes`
+    Puppet.info("upguard: node_create response=#{response}")
     node = JSON.load(response)
     if node["id"]
       node["id"]
     else
-      Puppet.notice("upguard: failed to create node: #{ip_hostname}")
+      Puppet.err("upguard: failed to create node: #{ip_hostname}")
+      raise StandardError, "upguard: unable to create node: confirm node parameters are correct"
     end
   end
   module_function :node_create
@@ -81,6 +90,7 @@ Puppet::Reports.register_report(:upguard) do
   # Kick off a node scan
   def node_scan(api_key, instance, node_id, tag)
     response = `curl -X POST -s -k -H 'Authorization: Token token="#{api_key}"' -H 'Accept: application/json' -H 'Content-Type: application/json' #{instance}/api/v2/nodes/#{node_id}/start_scan.json?label=#{tag}`
+    Puppet.info("upguard: node_scan response=#{response}")
     JSON.load(response)
   end
   module_function :node_scan

2d753b7e3153ac0725e9dddbac07c95eb0aa0ab2
Julianne Gendrano <jujukoo@gmail.com>
2016-07-27 10:24:23 -0700

    removed ignoring self-signed certs lines

--- code/powershell/query_vuln_endpoint.ps1
+++ code/powershell/query_vuln_endpoint.ps1
@@ -3,16 +3,4 @@ $secretKey = "[enter secret key here]"
 $headers = @{Authorization = 'Token token="' + $apiKey + $secretKey + '"'}
 
 $vulnScanURI = "https://app.upguard.com/api/v2/vulns.json"
-add-type @"
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-        public bool CheckValidationResult(
-            ServicePoint srvPoint, X509Certificate certificate,
-            WebRequest request, int certificateProblem) {
-            return true;
-        }
-    }
-"@
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 Invoke-RestMethod -Method "GET" -Uri $vulnScanURI -Headers $headers

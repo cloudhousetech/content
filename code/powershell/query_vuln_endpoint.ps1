2476d5930792adb61d4351f079d4345110aa435f
Julianne Gendrano <jujukoo@gmail.com>
2016-08-25 21:54:59 -0700

    indentation

--- code/powershell/query_vuln_endpoint.ps1
+++ code/powershell/query_vuln_endpoint.ps1
@@ -6,9 +6,9 @@ $vulnScanURI = "https://app.upguard.com/api/v2/vulns.json?page="
 $counter = 1
 
 do {
-$name = $vulnScanURI + $counter
-$result = Invoke-RestMethod -Method "GET" -Uri $name -Headers $headers
-$counter += 1
-echo $result
+    $name = $vulnScanURI + $counter
+    $result = Invoke-RestMethod -Method "GET" -Uri $name -Headers $headers
+    $counter += 1
+    echo $result
 }
 until($result.length -eq 0)

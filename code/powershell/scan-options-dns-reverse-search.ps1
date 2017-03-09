59554608d48c25cfe7d784c64738c37992f2f18c
Alistair Pialek <alistair.pialek@upguard.com>
2017-03-09 12:11:53 -0800

    dns-reverse-search

---
+++ code/powershell/scan-options-dns-reverse-search.ps1
@@ -0,0 +1,15 @@
+try {
+    $dnsObj = @()
+    $dns_results = (gwmi -query "select * from win32_networkadapterconfiguration where ipenabled = true").dnsserversearchorder
+    $dns_results_count = 0
+    foreach ($dns_value in $dns_results) {
+        $tempObj = New-Object -TypeName PSObject
+        $dns_results_count += 1
+        $dns_key = "dnsReverseSearchEntry" + $dns_results_count
+        $tempObj | Add-Member -MemberType NoteProperty –Name $dns_key –Value $dns_value
+        $dnsObj += $tempObj
+    }
+    $dnsObj
+} catch {
+    "Exception encountered: " + $_.Exception.Message
+}

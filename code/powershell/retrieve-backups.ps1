9f0c2e5233ae2b9d75dc231330f367932f5aa8c7
Alistair Pialek <alistair.pialek@upguard.com>
2017-06-29 14:29:07 -0700

    Use WebClient instead

--- code/powershell/retrieve-backups.ps1
+++ code/powershell/retrieve-backups.ps1
@@ -7,8 +7,10 @@ $backup_location = "C:\Backups\"
 $backup_file = Get-Date -Format yyyy-MM-ddTHH-mm-ss
 $backup_filepath = $backup_location + $backup_file + ".tar.gz"
 
-# Download the latest appliance backup.
-Invoke-WebRequest -Uri $backup_endpoint -OutFile $backup_filepath -Credential $credentials
+# Download the latest appliance backup. WebClient is used here so that streaming is buffered to disk rather than to memory.
+$webclient = New-Object System.Net.WebClient
+$Webclient.Credentials = New-Object System.Net.Networkcredential($username, $password)
+$webclient.DownloadFile($backup_endpoint, $backup_filepath)
 
 # Only keep the last two backups. There is no need to store backups beyond this.
 $backup_files = Get-ChildItem $backup_location -Filter *.tar.gz | Sort LastWriteTime -Descending

fe6acc0a7429fc5f48bff05da1ea521fc8b2f189
colinfong <colin.fong@upguard.com>
2017-09-21 11:09:55 -0700

    Updated comments and url setup

--- code/api/ruby/list_node_groups.rb
+++ code/api/ruby/list_node_groups.rb
@@ -25,10 +25,9 @@ end
 opt_parser.parse!
 
 #Authorization
-scheme     = 'https' #scheme can be https or http
-api_key    = '' #secret key shown when API enabled in Manage Accounts | Account | enable api access
-secret_key = '' #service Api key under Manage Accounts | Account 
-url        = '' #Example: 123.0.0.1
+url        = '' #Example: https://123.0.0.1 or http://<my-server>.com
+api_key    = '' #service API key under Manage Accounts | Account 
+secret_key = '' #secret key shown when API enabled in Manage Accounts | Account | enable api access
 
 header     = {"Authorization" => "Token token=\"#{api_key}#{secret_key}\""}
 page       = 1
@@ -41,7 +40,7 @@ OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = :TLSv1_2
 
 #Paginate API requests
 while !response.nil? and response.count == per_page
-  link = "#{scheme}://#{url}/api/v2/node_groups.json?page=#{page.to_s}&per_page=#{per_page.to_s}"
+  link = "#{url}/api/v2/node_groups.json?page=#{page.to_s}&per_page=#{per_page.to_s}"
   puts "Attempting to invoke #{link}"
   response = HTTParty.get(link, headers: header, :verify => options[:SSL_cert])
   if response.code != 200

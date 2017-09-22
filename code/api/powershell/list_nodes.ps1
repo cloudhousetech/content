e2e70db612ff6123b41dac4b7f4f078152b7d6fa
colinfong <colinjoefong@gmail.com>
2017-09-21 19:28:38 -0700

    Fixed spacing on variables, formatted intro, cleaned while condition

--- code/api/powershell/list_nodes.ps1
+++ code/api/powershell/list_nodes.ps1
@@ -1,64 +1,41 @@
 ﻿Param(    
-    [switch]$disableSSLCert,
+    [switch]$disableSSL,
     [switch]$disableDebugOutput,
+    [switch]$printJSON,
     [string]$status,
-    [string]$lastScanStatus,
-    [string]$outputFormat
+    [string]$lastScanStatus
 )
 
-$scheme = 'https' #scheme can be https or http
-$secret_key = '' #secret key shown when API enabled in Manage Accounts | Account | enable api access
-$api_key = '' #service Api key under Manage Accounts | Account 
-$url = '' #Example: 123.0.0.1
+# Authorization
+$url =        '' # Example: https://123.0.0.1 or http://<my-server>.com
+$api_key =    '' # Service Api key under Manage Accounts | Account
+$secret_key = '' # Secret key shown when API enabled in Manage Accounts | Account | enable API access
 
+# Added serializer for parsing large JSON responses
 Add-Type -AssemblyName System.Web.Extensions
 $json = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
 $json.MaxJsonLength = 104857600 #100mb as bytes, default is 2mb
 
-
-<#
-This script lists the nodes stored in UpGuard databases.
-The resulting API response is stored in the $nodeArr variable which is an array of hashmaps, each holding a single node's information.
-
-UpGuard Support Site Documentation: https://support.upguard.com/upguard/nodes-api-v2.html#index
-
-Usage Instructions:
-  - Machine must be running Powershell 3 and up
-  - Please provide the secret_key, api_key, url, and appropriate scheme of your UpGuard Appliance
-    in the fields above in order to run this script.
-    
-Optional API Query Input Flags: 
-  - status: Filters node list by active, deleted, or detected status.
-    Inputs: active, deleted, detected
-  - lastScanStatus: Filters node list by result of the last scan of that node.
-    Inputs: success, failure, offline, timeout, error, exception, all_failures
-
-Optional Script Flags:
-  - outputFormat: Determinds the output format of nodes. Outputs to console by default
-    Inputs: json
-  - disableSSLCert: Disables SSL Certificate check for the API call.
-  - disableDebugOutput: Disables debugging output.
-
-Example Usage:
-  - List all nodes without requiring an SSLCertificate check:
-    ./path_to_script/list_nodes.ps1 -disableSSLCert
-  - List all nodes that are active and have had a succesful last scan:
-    ./path_to_script/list_nodes.ps1 -status active -lastScanStatus success all_failures
-
-Example Script Output:
-  Name:              First Node
-  Node Id:           1
-  Environment Id:    2
-  Node Type:         SV
-  Mac Address:       02:c3:89:c0:82:19
-  IP Address:        0.0.0.0
-  Short Description: The first node added
-  OS Family Id:      2
-  OS ID:             2
-  External Id:       i-ac49774
-  Online:            True
-  Url:               http://192.168.88.1:3000/api/v2/nodes/1
-#>
+# Usage Instructions:
+#   - To use, please input the Authorization parameters above for URL, api_key, and secret_key
+#   - Minimum Requirements: Powershell 3 and up
+#    
+# Optional API Query Input Flags: 
+#   - status: Filters node list by active, deleted, or detected status.
+#     Inputs: active, deleted, detected
+#   - lastScanStatus: Filters node list by result of the last scan of that node.
+#     Inputs: success, failure, offline, timeout, error, exception, all_failures
+#
+# Optional Script Flags:
+#   - returnJSON: Returns results in JSON format
+#   - disableSSLCert: Disables SSL Certificate check for the API call.
+#   - disableDebugOutput: Disables debugging output.
+#
+# Example Usage:
+#   - List all nodes without requiring an SSLCertificate check:
+#     ./path_to_script/list_nodes.ps1 -disableSSLCert
+#   - List all nodes that are active and have had a succesful last scan:
+#     ./path_to_script/list_nodes.ps1 -status active -lastScanStatus success
 
 function Write-OptionalDebug {
     param([string]$output)
@@ -67,23 +44,18 @@ function Write-OptionalDebug {
     }
 }
 
-function Set-SslCertificateValidation
-{
-  <#
-    .SYNOPSIS
-    This function enables or disables SSL Cert validation in your PowerShell session.  Calling this affects SSL validation for ALL function calls in the session!
-    
-    .PARAMETER Disable
-    If specified, validation is disabled.  If not specified (the default) validation is re-enabled.
-    
-    .EXAMPLE
-    Set-SslCertificateValidation -Disable
-    #  Disables SSL Cert validation
-
-    .EXAMPLE
-    Set-SslCertificateValidation
-    #  Re-enables SSL Cert validation again
-  #>
+function Set-SslCertificateValidation {
+# This function enables or disables SSL Cert validation in your PowerShell session.  Calling this affects SSL validation for ALL function calls in the session!
+#    
+# Optional Flag
+#   - Disable: If specified, validation is disabled.  If not specified (the default) validation is re-enabled.
+#    
+# Examples
+#   - Disable SSL Cert validation
+#     Set-SslCertificateValidation -Disable
+#
+#   - Re-enable SSL Cert validation
+#     Set-SslCertificateValidation
     param
     ([switch] $Disable)
     $type = [AppDomain]::CurrentDomain.GetAssemblies().ExportedTypes | Where-Object { $_.Name -ieq "TrustAllCertsPolicy" }
@@ -112,13 +84,13 @@ function Set-SslCertificateValidation
 
 function Invoke-UpGuardApi {
     param(
-        [string]$Url = $url,
-        [string]$SecretKey = $secret_key,
-        [string]$ApiKey = $api_key,
+        [string]$Url         = $url,
+        [string]$SecretKey   = $secret_key,
+        [string]$ApiKey      = $api_key,
         [string]$RequestPath = $null,
-        [string]$FullPath = $null,
-        [string]$Scheme = $scheme,
-        [string]$Method = "GET"
+        [string]$FullPath    = $null,
+        [string]$Scheme      = $scheme,
+        [string]$Method       = "GET"
     )
 
     if ($RequestPath -eq $null -and $FullPath -eq $null) {
@@ -136,7 +108,7 @@ function Invoke-UpGuardApi {
         }
         $uri = $FullPath
     } else {
-        $uri = "$($Scheme)://$Url/$RequestPath"
+        $uri = "$Url/$RequestPath"
     }
     
     Write-OptionalDebug "Attempting to invoke $uri with $Method"
@@ -154,31 +126,27 @@ function Invoke-UpGuardApi {
         break
     }
     $content = $req.Content
-    $nodes = $json.Deserialize($content, [System.Object])
+    $nodes   = $json.Deserialize($content, [System.Object])
     $nodes
 }
 
-
-
-
 function Start-Main {
-    #SSL Certificate Check
-    if($disableSSLCert) {
+    # SSL Certificate Check
+    if($disableSSL) {
         Set-SslCertificateValidation -Disable
         Write-OptionalDebug "SSL Certificate checking is disabled"
     } else {
         Write-OptionalDebug "SSL Certificate checking is enabled"
     }
 
-    #Encryption Protocol Setting
+    # Encryption Protocol Setting
     Write-OptionalDebug "Enforcing TLS 1.2"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
     
-    $page = 1
-    $perPage = 500
+    $page      = 1
+    $perPage   = 500
     $nodeCount = $perPage
-    $nodeArr = @() #Nodes are stored in an array for reuse
+    $nodeArr   = @() # Nodes are stored in an array for reuse
 
     $statusString = ""
     if($status -ne "") {
@@ -195,22 +163,22 @@ function Start-Main {
         $nodeCount = $nodes.Length
         ForEach($node in $nodes) {
             $nodeArr += @{"name" = $node."name";
-            "id" = $node."id";
-            "environment_id" = $node."environment_id";
-            "node_type" = $node."node_type";
-            "mac_address" = $node."mac_address";
-            "ip_address" = $node."ip_address";
-            "short_description" = $node."short_description";
+            "id"                         = $node."id";
+            "environment_id"             = $node."environment_id";
+            "node_type"                  = $node."node_type";
+            "mac_address"                = $node."mac_address";
+            "ip_address"                 = $node."ip_address";
+            "short_description"          = $node."short_description";
             "operating_system_family_id" = $node."operating_system_family_id";
-            "operating_system_id" = $node."operating_system_id";
-            "external_id" = $node."external_id";
-            "online" = $node."online"
-            "url" = $node."url"}
+            "operating_system_id"        = $node."operating_system_id";
+            "external_id"                = $node."external_id";
+            "online"                     = $node."online"
+            "url"                        = $node."url"}
         }
         $page++
     }
 
-    if($outputFormat -eq "json") {
+    if($printJSON) {
         $json.Serialize($nodeArr)
     } else {
         Write-OptionalDebug "Retrieved $($nodeArr.count) nodes`n"

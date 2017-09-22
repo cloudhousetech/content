8ce94c552f7c08a64a2e3080d248548d8fbfc189
colinfong <colin.fong@upguard.com>
2017-09-21 17:53:26 -0700

    switched camel casing to snake case, fixed variable spacing, fixed while loop condition

--- code/api/ruby/list_node_groups.rb
+++ code/api/ruby/list_node_groups.rb
@@ -34,29 +34,28 @@ url        = '' # Example: https://123.0.0.1 or http://<my-server>.com
 api_key    = '' # Service API key under Manage Accounts | Account 
 secret_key = '' # Secret key shown when API enabled in Manage Accounts | Account | enable API access
 
-header     = {"Authorization" => "Token token=\"#{api_key}#{secret_key}\""}
-page       = 1
-per_page   = 100
+header       = {"Authorization" => "Token token=\"#{api_key}#{secret_key}\""}
+page         = 1
+per_page     = 100
+node_groups   = Array.new
+response     = nil
 api_endpoint = 'node_groups.json'
 if options[:node_group_id] != nil
   api_endpoint = "node_groups/#{options[:node_group_id]}/nodes.json"
 end
 
-response = Array.new(per_page)
-nodeGroups = Array.new
-
 # TLS 1.2
 OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = :TLSv1_2
 
 # Paginate API requests
-while !response.nil? and response.count == per_page
+while response.nil? || response.count == per_page
   link = "#{url}/api/v2/#{api_endpoint}?page=#{page.to_s}&per_page=#{per_page.to_s}"
   puts "Attempting to invoke #{link}"
   response = HTTParty.get(link, headers: header, :verify => options[:SSL_cert])
   if response.code != 200
     puts "Error, server responded with: #{response.code} - #{response["error"]}"
   else
-    nodeGroups += response
+    node_groups += response
   end
 
   page += 1
@@ -64,8 +63,8 @@ end
 
 # Print Results
 if options[:node_group_id] != nil
-  puts "Retrieved #{nodeGroups.count} nodes from node group #{options[:node_group_id]}"
+  puts "Retrieved #{node_groups.count} nodes from node group #{options[:node_group_id]}"
 else
-  puts "Retreived #{nodeGroups.count} node groups"
+  puts "Retreived #{node_groups.count} node groups"
 end
-puts JSON.pretty_generate(nodeGroups)
+puts JSON.pretty_generate(node_groups)

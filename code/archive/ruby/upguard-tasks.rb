a2ee20dcb470accb10b65a6a5ebb6050cbf1d816
Alistair Pialek <alistair.pialek@upguard.com>
2017-10-05 15:59:13 -0700

    remove_os_from_node_group

--- code/archive/ruby/upguard-tasks.rb
+++ code/archive/ruby/upguard-tasks.rb
@@ -5,17 +5,24 @@ def main
 
   # This method is used to update the username and password for nodes belonging to a particular connection
   # manager group and operating system family.
-  node_username = 'username'
-  node_password = 'password'
-  cm_group_id   = 123
-  os_family     = 'windows'
-  ug.update_creds_per_cmg_and_osf(node_username, node_password, cm_group_id, os_family)
+  # node_username = 'username'
+  # node_password = 'password'
+  # cm_group_id   = 123
+  # os_family     = 'windows'
+  # ug.update_creds_per_cmg_and_osf(node_username, node_password, cm_group_id, os_family)
+
+  # This method will remove nodes of a particular operating system from a node group.
+  # E.g. Remove all Windows 2008 nodes from the "Windows Mixed" node group as this group
+  # should only container Windows 2012 nodes.
+  # os_id = 125 # 125 = Windows 2012, 123 = Windows 2008,
+  # node_group_id = 83
+  # ug.remove_os_from_node_group(os_id, node_group_id)
 end
 
 class UpGuard
 
-  $version     = '0.0.1'
-  $test_mode   = false
+  $version     = '0.0.2'
+  $test_mode   = true
   $script_name = 'upguard'
   $api_key     = 'YOUR API KEY'
   $secret_key  = 'YOUR SECRET KEY'
@@ -30,9 +37,47 @@ class UpGuard
   # Endpoints
   @nodes_index_endpoint = "#{$website}/api/v2/nodes?page={{page_number}}&per_page={{per_page_number}}"
   @nodes_show_endpoint  = "#{$website}/api/v2/nodes/{{id}}"
+  @nodes_remove_node_group = "#{$website}/api/v2/nodes/{{node_id}}/remove_from_node_group.json?node_group_id={{node_group_id}}"
+  @node_group_nodes_endpoint = "#{$website}/api/v2/node_groups/{{node_group_id}}/nodes.json?page={{page_number}}&per_page={{per_page_number}}"
 
   class << self
-    attr_accessor :nodes_index_endpoint, :nodes_show_endpoint
+    attr_accessor :nodes_index_endpoint, :nodes_show_endpoint, :node_group_nodes_endpoint, :nodes_remove_node_group
+  end
+
+  def remove_os_from_node_group(os_id, node_group_id)
+    page = 1
+    all_nodes = []
+    found_no_nodes = true
+
+    some_nodes = get_more_node_group_nodes(node_group_id, page)
+    all_nodes += some_nodes
+    while !some_nodes.nil? && some_nodes.any?
+      page += 1
+      some_nodes = get_more_node_group_nodes(node_group_id, page)
+      all_nodes += some_nodes
+    end
+
+    all_nodes.each do |node|
+      working_url = UpGuard.nodes_show_endpoint.sub('{{id}}', node['id'].to_s)
+      response = Client.get(working_url)
+      node_detail = JSON.load(response.body)
+      if node_detail['operating_system_id'] == os_id
+        found_no_nodes = false
+        if $test_mode
+          puts "#{log_prefix} would remove #{node['name']}"
+          next
+        else
+          working_url = UpGuard.nodes_remove_node_group.sub('{{node_id}}', node['id'].to_s)
+          working_url = working_url.sub('{{node_group_id}}', node_group_id.to_s)
+          response = Client.post(working_url)
+          puts "#{log_prefix} removed #{node['name']}: '#{response.body}'"
+        end
+      end
+    end
+
+    if found_no_nodes
+      puts "#{log_prefix} no nodes found to remove"
+    end
   end
 
   def update_creds_per_cmg_and_osf(username, password, cmg_id, osf_name)
@@ -72,10 +117,10 @@ class UpGuard
           puts "#{log_prefix} updated #{node['name']}: '#{response.body}'"
         end
       end
+    end
 
-      if found_no_nodes
-        puts "#{log_prefix} no nodes found to update"
-      end
+    if found_no_nodes
+      puts "#{log_prefix} no nodes found to update"
     end
   end
 
@@ -99,6 +144,15 @@ class UpGuard
     per_page = 50
     working_url = UpGuard.nodes_index_endpoint.sub('{{page_number}}', page.to_s)
     working_url = working_url.sub('{{per_page_number}}', per_page.to_s)
+    response = Client.get(working_url)
+    JSON.parse(response.body)
+  end
+
+  def get_more_node_group_nodes(node_group_id, page)
+    per_page = 50
+    working_url = UpGuard.node_group_nodes_endpoint.sub('{{page_number}}', page.to_s)
+    working_url = working_url.sub('{{per_page_number}}', per_page.to_s)
+    working_url = working_url.sub('{{node_group_id}}', node_group_id.to_s)
     response = Client.get(working_url)
     JSON.parse(response.body)
   end

81c33e4f4ec4216519851d8c6986f61bb842eeb0
colinfong <colin.fong@upguard.com>
2017-10-19 11:54:23 -0700

    Added debug suppression by default and made json output optional + regular output by default

--- code/api/ruby/list_environements.rb
+++ code/api/ruby/list_environements.rb
@@ -12,30 +12,49 @@ require 'optparse'
 #   --env_name: Lists an environment's environment ID given it's name
 #
 # Optional Flags:
-#   --disable-ssl: Disables SSL certificate verification
+#   --print_json: Prints API results in JSON format
+#   --debug_output: Enables script debugging output
+#   --disable_ssl: Disables SSL certificate verification
 #
 # Usage Example(s):
 #   ./<path-to-script>/list_environments.rb
 #   ./<path-to-script>/list_environments.rb --env_id 4
 #   ./<path-to-script>/list_environments.rb --env_name 'Duplicate Nodes'
 
+# Optional Debug
+def write_optional_debug (str="")
+  if $options[:debug_output]
+    puts str
+  end
+end
+
 # Flag Parsing
-options            = {}
-options[:SSL_cert] = true
-options[:env_id]   = nil
-options[:env_name] = nil
+$options               = {}
+$options[:SSL_cert]    = true
+$options[:env_id]      = nil
+$options[:env_name]    = nil
+$options[:print_json]  = false
+$options[:debug_output] = false 
 
 opt_parser = OptionParser.new do |opt|
+  opt.on('--print_json') do
+    $options[:print_json] = true
+  end
+
+  opt.on('--debug_output') do
+    $options[:debug_output] = true
+  end
+
   opt.on('--disable_ssl') do
-    options[:SSL_cert] = false
+    $options[:SSL_cert] = false
   end
 
   opt.on('--env_id=ENVIRONMENTID') do |e|
-    options[:env_id] = e
+    $options[:env_id] = e
   end
 
   opt.on('--env_name=ENVIRONMENTNAME') do |name|
-    options[:env_name] = name
+    $options[:env_name] = name
   end 
 end
 
@@ -52,33 +71,34 @@ per_page     = 100
 environments = Array.new
 response     = nil
 
-api_endpoint = 'environments.json'
-if options[:env_id] != nil
-  api_endpoint = "environments/#{options[:env_id]}/nodes.json"
-end
-
 # TLS 1.2
 OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = :TLSv1_2
 
-if options[:env_name] != nil
+if $options[:env_name] != nil
   # Retrieve environment ID from environment name
-  link = "#{url}/api/v2/environments/lookup.json?name=#{options[:env_name].gsub(" ", "+")}"
-  puts "Attempting to invoke #{link}"
-  response = HTTParty.get(link, headers: header, :verify => options[:SSL_cert])
+  link = "#{url}/api/v2/environments/lookup.json?name=#{$options[:env_name].gsub(" ", "+")}"
+  write_optional_debug("Attempting to invoke #{link}")
+  response = HTTParty.get(link, headers: header, :verify => $options[:SSL_cert])
   if response.code != 200
-    puts "Error, server responded with: #{response.code} - #{response["error"]}"
+    write_optional_debug("Error, server responded with: #{response.code} - #{response["error"]}")
   else
-    puts "Environment Name: #{options[:env_name]}"
+    puts "Environment Name: #{$options[:env_name]}"
     puts "Environment ID: #{response["environment_id"]}"
   end
 else
+  # Show environments or an evironment's nodes
+  api_endpoint = 'environments.json'
+  if $options[:env_id] != nil
+    api_endpoint = "environments/#{$options[:env_id]}/nodes.json"
+  end
+
   # Paginate API Requests
   while response.nil? || response.count == per_page
     link = "#{url}/api/v2/#{api_endpoint}?page=#{page.to_s}&per_page=#{per_page.to_s}"
-    puts "Attempting to invoke #{link}"
-    response = HTTParty.get(link, headers: header, :verify => options[:SSL_cert])
+    write_optional_debug("Attempting to invoke #{link}")
+    response = HTTParty.get(link, headers: header, :verify => $options[:SSL_cert])
     if response.code != 200
-      puts "Error, server responded with: #{response.code} - #{response["error"]}"
+      write_optional_debug("Error, server responded with: #{response.code} - #{response["error"]}")
     else
       environments += response
     end
@@ -87,11 +107,15 @@ else
   end
 
   # Print Results
-  if options[:env_id] != nil
-    puts "Retreived #{environments.count} nodes from environment #{options[:env_id]}" 
+  if $options[:env_id] != nil
+    write_optional_debug("Retreived #{environments.count} nodes from environment #{$options[:env_id]}")
   else 
-    puts "Retreived #{environments.count} environments"
+    write_optional_debug("Retreived #{environments.count} environments")
   end
-  puts JSON.pretty_generate(environments)
+  if $options[:print_json]
+    puts JSON.pretty_generate(environments)
+  else
+    puts environments
+  end
 end
 
